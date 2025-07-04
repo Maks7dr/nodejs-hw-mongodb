71a1e387032c7badfbff51b87545f0747e1ba43b
Maksym Drachenko <maks7d@gmail.com>
2025-07-04 20:04:59 +0500

    add 6

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-import 'dotenv/config';
-
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import contactsRouter from './routes/contactsRouter.js';
 
 export function setupServer() {
   const app = express();
 
   app.use(cors());
   app.use(pino());
+
+  app.use('/contacts', contactsRouter);
 
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
